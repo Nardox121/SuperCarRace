83bb4b4d126fae86db1f392ce4f295b181636aa5
GeeA02 <43759867+GeeA02@users.noreply.github.com>
2020-04-04 18:11:25 +0200

    Added Car

--- run.py
+++ run.py
@@ -1,8 +1,29 @@
 import pygame, sys
+from Car import Car
 
-screen = pygame.display.set_mode((512, 512))
+pygame.init()
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((512, 500))
+car = Car(10,10)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+            
+    pressedKeys = pygame.key.get_pressed()
+
+    if pressedKeys[pygame.K_RIGHT]:
+        car.moveRight()
+    if pressedKeys[pygame.K_LEFT]:
+        car.moveLeft()
+    if pressedKeys[pygame.K_UP]:
+        car.moveUp()
+    if pressedKeys[pygame.K_DOWN]:
+        car.moveDown()
+
+
+    screen.fill((0,0,0))
+    pygame.draw.rect(screen, (245,25,25), car.getCar())
+    pygame.display.flip()
+    clock.tick(60)
