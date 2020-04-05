64005a24418da02dc6ddf84f48b6d3a119dbf039
Adrian Szarnowski <nynu88888888@gmail.com>
2020-04-05 13:47:01 +0200

    Ej Gabi zoba jaki syf zrobiłem

--- Car.py
+++ Car.py
@@ -1,28 +1,30 @@
 import pygame, math
+from pygame.math import Vector2
 
 class Car:
-    def __init__(self, xpos, ypos, w, h):
-        self.car = pygame.Rect(xpos, ypos, 20,20)
-        self.speed = 5
-        self.screenWidth = w - 20
-        self.screenHeight = h - 20
+    def __init__(self, x, y, angle = 90, length = 2, max_steering = 30, max_acceleration= 5.0):
+        self.position = Vector2(x, y)
+        self.velocity = Vector2(0.0, 0.0)
+        self.angle = angle
+        self.length = length
+        self.max_acceleration = max_acceleration
+        self.max_steering = max_steering
+        self.max_velocity = 10
+        self.brake_deceleration = 10
+        self.free_deceleration = 3
 
-    def moveUp(self):
-        if self.car.y > 1:
-            self.car.y -= self.speed
-        
-    def moveDown(self):
-        if self.car.y < self.screenHeight:
-            self.car.y += self.speed
+        self.acceleration = 0.0
+        self.steering = 0.0
 
-    def moveRight(self):
-        if self.car.x < self.screenWidth:
-            self.car.x += self.speed
+    def update(self, dt):
+        self.velocity += (self.acceleration * dt, 0)
+        self.velocity.x = max(-self.max_velocity, min(self.velocity.x, self.max_velocity))
 
-    def moveLeft(self):
-        if self.car.x > 1:
-            self.car.x -= self.speed
+        if self.steering:
+            turning_radius = self.length / math.sin(math.radians(self.steering))
+            angular_velocity = self.velocity.x / turning_radius
+        else:
+            angular_velocity = 0
 
-
-    def getCar(self):
-        return self.car
+        self.position += self.velocity.rotate(-self.angle) * dt
+        self.angle += math.degrees(angular_velocity) * dt
--- run.py
+++ run.py
@@ -1,30 +1,64 @@
-import pygame, sys
+import pygame
+import sys
+from math import copysign
 from Car import Car
 
-width = 512
-height = 500
+width = 1280
+height = 720
 pygame.init()
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((width, height))
-car = Car(10, 10, width, height)
+car = Car(10, 10)
+clock = pygame.time.Clock()
+ticks = 60
+
+car_image = pygame.transform.scale(pygame.image.load("Car.png"), (20, 20))
 
 while True:
+    dt = clock.get_time() / 500
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-            
-    pressedKeys = pygame.key.get_pressed()
 
-    if pressedKeys[pygame.K_RIGHT]:
-        car.moveRight()
-    if pressedKeys[pygame.K_LEFT]:
-        car.moveLeft()
-    if pressedKeys[pygame.K_UP]:
-        car.moveUp()
-    if pressedKeys[pygame.K_DOWN]:
-        car.moveDown()
+    pressed = pygame.key.get_pressed()
+
+    if pressed[pygame.K_UP]:
+        if car.velocity.x < 0:
+            car.acceleration = car.brake_deceleration
+        else:
+            car.acceleration += 1 * dt
+    elif pressed[pygame.K_DOWN]:
+        if car.velocity.x > 0:
+            car.acceleration = -car.brake_deceleration
+        else:
+            car.acceleration -= 1 * dt
+    elif pressed[pygame.K_SPACE]:
+        if abs(car.velocity.x) > dt * car.brake_deceleration:
+            car.acceleration = -copysign(car.brake_deceleration, car.velocity.x)
+        else:
+            car.acceleration = -car.velocity.x / dt
+    else:
+        if abs(car.velocity.x) > dt * car.free_deceleration:
+            car.acceleration = -copysign(car.free_deceleration, car.velocity.x)
+        else:
+            if dt != 0:
+                car.acceleration = -car.velocity.x / dt
+    car.acceleration = max(-car.max_acceleration, min(car.acceleration, car.max_acceleration))
+
+    if pressed[pygame.K_RIGHT]:
+        car.steering -= 30 * dt
+    elif pressed[pygame.K_LEFT]:
+        car.steering += 30 * dt
+    else:
+        car.steering = 0
+    car.steering = max(-car.max_steering, min(car.steering, car.max_steering))
     
-    screen.fill((0,0,0))
-    pygame.draw.rect(screen, (245,25,25), car.getCar())
+    car.update(dt)
+
+    ppu = 32
+    screen.fill((255, 255, 255))
+    rotated = pygame.transform.rotate(car_image, car.angle)
+    rect = rotated.get_rect()
+    screen.blit(rotated, car.position * ppu - (rect.width / 2, rect.height / 2))
     pygame.display.flip()
-    clock.tick(60)
+    clock.tick(ticks)
