e258ffe346d2d49502c0d4e10eefb7f3171baf0c
GeeA02 <43759867+GeeA02@users.noreply.github.com>
2020-06-02 15:42:23 +0200

    basic AI structure

--- AI.py
+++ AI.py
@@ -1,159 +1,111 @@
-import numpy
-import GeneticAlgorithm
+import pygame
+import random
+import os
+import time
+import neat
 import pickle
-import NeuralNetwork
+import CarAI
 
-class AI(self):
-    def __init__(self, data_inputs, data_outputs):
-        self.sol_per_pop = 8
-        self.num_parents_mating = 4
-        self.num_generations = 1000
-        self.mutation_percent = 10
-        self.initial_pop_weights = []
-        for curr_sol in numpy.arange(0, self.sol_per_pop):
-            self.HL1_neurons = 6
-            self.input_HL1_weights = numpy.random.uniform(low=-0.1, high=0.1, 
+def eval_genomes(genomes, config):
+    """
+    runs the simulation of the current population of
+    birds and sets their fitness based on the distance they
+    reach in the game.
+    """
+    global WIN, gen
+    win = WIN
+    gen += 1
 
-                                                    size=(data_inputs.shape[1], HL1_neurons))
+    # start by creating lists holding the genome itself, the
+    # neural network associated with the genome and the
+    # bird object that uses that network to play
+    nets = []
+    cars = []
+    ge = []
+    for genome_id, genome in genomes:
+        genome.fitness = 0  # start with fitness level of 0
+        net = neat.nn.FeedForwardNetwork.create(genome, config)
+        nets.append(net)
+        cars.append(CarAI(PARAMETRY))
+        ge.append(genome)
 
-            self.HL2_neurons = 5
-            self.HL1_HL2_weights = numpy.random.uniform(low=-0.1, high=0.1, 
+    score = 0
 
-                                                    size=(self.HL1_neurons, self.HL2_neurons))
+    clock = pygame.time.Clock()
 
-            self.output_neurons = 4
-            self.HL2_output_weights = numpy.random.uniform(low=-0.1, high=0.1, 
+    run = True
+    while run and len(cars) > 0:
+        clock.tick(30)
 
-                                                    size=(self.HL2_neurons, self.output_neurons))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+                quit()
+                break
 
-            self.initial_pop_weights.append(numpy.array([self.input_HL1_weights, 
+        for x, car in enumerate(cars):  # give each bird a fitness of 0.1 for each frame it stays alive
+            ge[x].fitness += 0.1
+            # send car position, distances from bbunds
+            output = nets[cars.index(car)].activate((PARAMETRY DO SIECI))
+            ###ruch auta hehe##########################################################
+            #car.move(output)
 
-                                                        self.HL1_HL2_weights, 
+        rem = []
+        ##  v tu ma być całe spawnowanie mapy v
 
-                                                       self.HL2_output_weights]))
+            if pipe.x + pipe.PIPE_TOP.get_width() < 0:
+                rem.append(pipe)
 
-        self.pop_weights_mat = numpy.array(self.initial_pop_weights)
-        self.pop_weights_vector = GeneticAlgorithm.mat_to_vector(self.pop_weights_mat)
-        self.best_outputs = []
-        self.accuracies = numpy.empty(shape=(self.num_generations))
+            if not pipe.passed and pipe.x < bird.x:
+                pipe.passed = True
+                add_pipe = True
 
-        for generation in range(self.num_generations):
-            print("Generation : ", generation)
+        if add_pipe:
+            score += 1
+            # can add this line to give more reward for passing through a pipe (not required)
+            for genome in ge:
+                genome.fitness += 5
+            pipes.append(Pipe(WIN_WIDTH))
 
-            # converting the solutions from being vectors to matrices.
-            self.pop_weights_mat = GeneticAlgorithm.vector_to_mat(self.pop_weights_vector, 
-                                            self.pop_weights_mat)
+        for r in rem:
+            pipes.remove(r)
 
-            # Measuring the fitness of each chromosome in the population.
-            self.fitness = NeuralNetwork.fitness(self.pop_weights_mat, 
-                                data_inputs, 
-                                data_outputs, 
-                                activation="sigmoid")
-
-            self.accuracies[generation] = self.fitness[0]
-
-f = open("dataset_features.pkl", "rb")
-data_inputs2 = pickle.load(f)
-f.close()
-features_STDs = numpy.std(a=data_inputs2, axis=0)
-data_inputs = data_inputs2[:, features_STDs>50]
+        for bird in birds:
+            if bird.y + bird.img.get_height() - 10 >= FLOOR or bird.y < -50:
+                nets.pop(birds.index(bird))
+                ge.pop(birds.index(bird))
+                birds.pop(birds.index(bird))
 
 
-f = open("outputs.pkl", "rb")
-data_outputs = pickle.load(f)
-f.close()
+        # break if score gets large enough
+        '''if score > 20:
+            pickle.dump(nets[0],open("best.pickle", "wb"))
+            break'''
 
-#Genetic algorithm parameters:
-#    Mating Pool Size (Number of Parents)
-#    Population Size
-#    Number of Generations
-#    Mutation Percent
 
-sol_per_pop = 8
-num_parents_mating = 4
-num_generations = 1000
-mutation_percent = 10
+def run(config_file):
+    """
+    runs the NEAT algorithm to train a neural network to play flappy bird.
+    :param config_file: location of config file
+    :return: None
+    """
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_file)
 
-#Creating the initial population.
-initial_pop_weights = []
-for curr_sol in numpy.arange(0, sol_per_pop):
-    HL1_neurons = 6
-    input_HL1_weights = numpy.random.uniform(low=-0.1, high=0.1, 
+    # Create the population, which is the top-level object for a NEAT run.
+    p = neat.Population(config)
 
-                                             size=(data_inputs.shape[1], HL1_neurons))
+    # Add a stdout reporter to show progress in the terminal.
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+    #p.add_reporter(neat.Checkpointer(5))
 
-    HL2_neurons = 5
-    HL1_HL2_weights = numpy.random.uniform(low=-0.1, high=0.1, 
+    # Run for up to 50 generations.
+    winner = p.run(eval_genomes, 50)
 
-                                             size=(HL1_neurons, HL2_neurons))
+    # show final stats
+    print('\nBest genome:\n{!s}'.format(winner))
 
-    output_neurons = 4
-    HL2_output_weights = numpy.random.uniform(low=-0.1, high=0.1, 
-
-                                              size=(HL2_neurons, output_neurons))
-
-    initial_pop_weights.append(numpy.array([input_HL1_weights, 
-
-                                                HL1_HL2_weights, 
-
-                                                HL2_output_weights]))
-
-pop_weights_mat = numpy.array(initial_pop_weights)
-pop_weights_vector = GeneticAlgorithm.mat_to_vector(pop_weights_mat)
-
-best_outputs = []
-accuracies = numpy.empty(shape=(num_generations))
-
-for generation in range(num_generations):
-    print("Generation : ", generation)
-
-    # converting the solutions from being vectors to matrices.
-    pop_weights_mat = GeneticAlgorithm.vector_to_mat(pop_weights_vector, 
-                                       pop_weights_mat)
-
-    # Measuring the fitness of each chromosome in the population.
-    fitness = NeuralNetwork.fitness(pop_weights_mat, 
-                          data_inputs, 
-                          data_outputs, 
-                          activation="sigmoid")
-
-    accuracies[generation] = fitness[0]
-    print("Fitness")
-    print(fitness)
-
-    # Selecting the best parents in the population for mating.
-    parents = GeneticAlgorithm.select_mating_pool(pop_weights_vector, 
-
-                                    fitness.copy(), 
-
-                                    num_parents_mating)
-    print("Parents")
-    print(parents)
-
-    # Generating next generation using crossover.
-    offspring_crossover = GeneticAlgorithm.crossover(parents,
-
-                                       offspring_size=(pop_weights_vector.shape[0]-parents.shape[0], pop_weights_vector.shape[1]))
-
-    print("Crossover")
-    print(offspring_crossover)
-
-    # Adding some variations to the offsrping using mutation.
-    offspring_mutation = GeneticAlgorithm.mutation(offspring_crossover, 
-
-                                     mutation_percent=mutation_percent)
-    print("Mutation")
-    print(offspring_mutation)
-
-    # Creating the new population based on the parents and offspring.
-    pop_weights_vector[0:parents.shape[0], :] = parents
-    pop_weights_vector[parents.shape[0]:, :] = offspring_mutation
-
-pop_weights_mat = GeneticAlgorithm.vector_to_mat(pop_weights_vector, pop_weights_mat)
-best_weights = pop_weights_mat [0, :]
-acc, predictions = NeuralNetwork.predict_outputs(best_weights, data_inputs, data_outputs, activation="sigmoid")
-print("Accuracy of the best solution is : ", acc)
-
-f = open("weights_"+str(num_generations)+"_iterations_"+str(mutation_percent)+"%_mutation.pkl", "wb")
-pickle.dump(pop_weights_mat, f)
-f.close()
