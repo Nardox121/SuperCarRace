c20c5d0d69ae538d62e0cbd2034c0abdf40e67f9
Nynu <adrianszarnowski8@gmail.com>
2020-06-02 17:15:10 +0200

    Changed the map structure

--- Map.py
+++ Map.py
@@ -1,19 +1,18 @@
 from pygame import draw, Color, Rect, image, display
 from PIL import Image
+from enum import Enum
 
 class Map:
     def __init__(self, mapPath):
         im = Image.open(mapPath, "r") 
         imageSizeW, imageSizeH = im.size
-        self.mapPixels = [(i, j) for i in range (1, imageSizeW) for j in range (1, imageSizeH) if im.getpixel((i, j)) != (255, 255, 255)]
-        self.map = image.load(mapPath).convert()
-        self.rectMap = [Rect((i, j), (1, 1)) for i, j in self.mapPixels]
-        
-        #points
-        self.pointLines = [((30, 60), (100, 1))]
-
-
+        self.map = [[MapTile.WALL if im.getpixel((i, j)) == (53, 53, 53) else MapTile.ROAD for i in range(1, imageSizeW)] for j in range(1, imageSizeH)]
+            
     def refresh(self, screen):
         screen.blit(self.map, [0,0])
         for dims in self.pointLines:
             draw.rect(screen, Color(0, 0, 255), Rect(dims))
+
+class MapTile(Enum):
+    WALL = 1
+    ROAD = 2
