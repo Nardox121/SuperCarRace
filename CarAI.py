b9c06c5caf543f2865a3fc3d9c6c7978e5488a5f
Nynu <adrianszarnowski8@gmail.com>
2020-06-04 21:08:22 +0200

    Updated distance viewing

--- CarAI.py
+++ CarAI.py
@@ -4,9 +4,10 @@ from pygame.math import Vector2
 from Car import Car, Action
 from Map import MapTile
 from pygame import draw,color
+from bresenham import bresenham
 
 class CarAI (Car):
-    def __init__(self, x, y, angle = 89.9, length = 0.5, max_steering = 15, max_acceleration= 2.5):
+    def __init__(self, x, y, angle = 90, length = 0.5, max_steering = 15, max_acceleration= 2.5):
         self.startPosition = Vector2(x, y)
         self.position = Vector2(self.startPosition.x, self.startPosition.y)
         self.velocity = Vector2(0.0, 0.0)
@@ -24,104 +25,68 @@ class CarAI (Car):
         self.dead = False
 
     def update_dist(self, screen, gameMap):
-        angles = [0, 45, -45, +90, -90, -180]
-        a = [0]*6
+        angles = [x for x in range(0, 360, 60)]
         for i in range(6):
-            a[i]=math.tan((self.angle+90+angles[i])/180*math.pi)
-        for i in range(6):
-            self.distances[i] = self.calculate_dist(a[i], self.angle+angles[i], screen, gameMap)
-        for i in range(6):
-            self.distances[i] = self.normalize(self.distances[i])
-        #print(self.distances)
+            self.distances[i] = self.normalize(self.calculate_dist(self.angle + angles[i], screen, gameMap))
 
-    def calculate_dist(self, a, angle, screen, gameMap):
-        if angle<0:
-            angle+=1080
-        if angle>=360:
-            angle=angle%360
-        if angle<90 and angle>=0:
-            quarter = 2
-        elif angle<180 and angle>=90:
-            quarter = 3
-        elif angle<270 and angle>=180:
-            quarter = 4
+    def calculate_dist(self, angle, screen, gameMap):
+        # Normalize it to 0-360 range
+        angle = round(angle % 360)
+
+        #position of the center point of our car
+        x = int(self.position.x*32)
+        y = int(self.position.y*32)
+        
+        dist = 0
+
+        #edge cases where tan returns 0 or is not defined
+        if angle == 0 or angle == 360:
+            while gameMap[y][x] != MapTile.WALL and dist < 150:
+                dist += 1
+                x += 1
+        elif angle == 90:
+            while gameMap[y][x] != MapTile.WALL and dist < 150:
+                dist += 1
+                y -= 1
+        elif angle == 180:
+            while gameMap[y][x] != MapTile.WALL and dist < 150:
+                dist += 1
+                x -= 1
+        elif angle == 270:
+            while gameMap[y][x] != MapTile.WALL and dist < 150:
+                dist += 1
+                y += 1
         else:
-            quarter = 1
+            a = math.tan(angle / 180 * math.pi)
 
-        x=int(self.position.x*32)
-        y=int(self.position.y*32)
-        count=0
-        if a==0:
-            a=0.001
-        if quarter == 1 and a<1:
-            a=1/a
-            while gameMap[y][x]!=MapTile.WALL :
-                x+=1
-                count+=1
-                y+=math.floor(count/a)
-                if count>=math.ceil(abs(a)):
-                    count=0
-        elif quarter == 1 and a>=1:
-            while gameMap[y][x]!=MapTile.WALL :
-                x+=1
-                count+=1
-                y+=math.floor(count/a)
-                if count>math.ceil(abs(a)):
-                    count=0
-        elif quarter == 2 and a>-1:
-            a=1/a
-            while gameMap[y][x]!=MapTile.WALL :
-                y-=1
-                count+=1
-                x-=math.ceil(count/a)
-                if count>math.ceil(abs(a)):
-                    count=0
-        elif quarter == 2 and a<=-1:
-            while gameMap[y][x]!=MapTile.WALL :
-                x+=1
-                count+=1
-                y+=math.ceil(count/a)
-                if count>math.ceil(abs(a)):
-                    count=0
-        elif quarter == 3 and a<1:
-            a=1/a
-            while gameMap[y][x]!=MapTile.WALL :
-                x-=1
-                count+=1
-                y-=math.floor(count/a)
-                if count>math.ceil(abs(a)):
-                    count=0
-        elif quarter == 3 and a>=1:
-            while gameMap[y][x]!=MapTile.WALL :
-                x-=1
-                count+=1
-                y-=math.floor(count/a)
-                if count>math.ceil(abs(a)):
-                    count=0
-        elif quarter == 4 and a>-1:
-            a=1/a
-            while gameMap[y][x]!=MapTile.WALL :
-                y+=1
-                count+=1
-                x-=math.ceil(count/a)
-                if count>math.ceil(abs(a)):
-                    count=0
-        elif quarter == 4 and a<=-1:
-            while gameMap[y][x]!=MapTile.WALL :
-                x-=1
-                count+=1
-                y-=math.ceil(count/a)
-                if count>math.ceil(abs(a)):
-                    count=0
-        pygame.draw.circle(screen, pygame.Color(255, 0, 0), (int(x),int(y)), 3)
-        return (math.sqrt((self.position.x*32-x)**2+(self.position.y*32-y)**2))
+            points = []
+            if 0 < angle < 90:
+                points = list(bresenham(x, y, x + 150, round(y - a * 150)))
+            elif 90 < angle < 180:
+                points = list(bresenham(x, y, x - 150, round(y + a * 150)))
+            elif 180 < angle < 270:
+                points = list(bresenham(x, y, x - 150, round(y + a * 150)))
+            elif 270 < angle < 360:            
+                points = list(bresenham(x, y, x + 150, round(y - a * 150)))
+            for point in points:
+                if dist > 150 or (1024, 600) < point < (0, 0):
+                    break
+                
+                if gameMap[y][x] == MapTile.WALL:
+                    break
+
+                x, y = point
+                dist = math.sqrt((self.position.x * 32 - x)**2 + (self.position.y * 32 - y)**2)
+
+        pygame.draw.circle(screen, pygame.Color(0, 0, 0), (int(x),int(y)), 3)
+        return dist
 
     def normalize(self, value):
         if value < 0:
             value = 0
-        if value > 200:
-            value = 200
-        return value / 200
+        if value > 150:
+            value = 150
+        return value / 150
     
     def move(self, dt, output):
         par = 0.5
