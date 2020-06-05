da03b5cc6dbfc72658341dff22140f4edf1288ad
GeeA02 <43759867+GeeA02@users.noreply.github.com>
2020-06-05 10:48:32 +0200

    changed try to if

--- run.py
+++ run.py
@@ -1,17 +1,18 @@
 import neat
 import pickle
+import os
 from game import withAI, withoutAI
 
 
 
 config_path = "assets/config.txt"
 winner_path = "winner.pkl"
-try:
+if(os.path.exists(winner_path)):
     config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
 
     with open(winner_path, "rb") as f:
         genome = pickle.load(f)
     genomes = [(1, genome)]
     withAI(genomes, config)
-except:
+else:
     withoutAI()
